712b5acce413a7f4819f20612458be402255b1cc
Cody Rush <rush_cody@hotmail.com>
2019-03-19 21:04:09 -0600

    add photos for deployment on netlify

---
+++ build/js/main.js
@@ -0,0 +1,26 @@
+
+let toggleNavStatus = false;
+let buttonClicked = false;
+
+let toggleNav = function() {
+    let getNav = document.querySelector(".mobile-menu");
+    
+    if (toggleNavStatus === false) {
+        getNav.style.top = "5rem";
+
+        toggleNavStatus = true;
+    }
+
+    else if (toggleNavStatus === true) {
+        getNav.style.top = "-100rem";
+
+        toggleNavStatus = false;
+    }
+
+}
+
+let closeMenu = function() {
+    let grabNav = document.querySelector(".mobile-menu");
+
+    grabNav.style.top = "-100rem";
+}
